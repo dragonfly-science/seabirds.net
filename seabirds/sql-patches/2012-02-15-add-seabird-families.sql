703098b8cf26c0c5a91622856555386455adbaa3
Edward Abraham <edward@dragonfly.co.nz>
2012-02-16 16:06:53 +1300

    Working on a test suite

--- seabirds/sql-patches/2012-02-15-add-seabird-families.sql
+++ seabirds/sql-patches/2012-02-15-add-seabird-families.sql
@@ -1,3 +1,4 @@
+BEGIN;
 CREATE TABLE "cms_image_seabird_families" (
     "id" serial NOT NULL PRIMARY KEY,
     "image_id" integer NOT NULL,
@@ -5,4 +6,4 @@ CREATE TABLE "cms_image_seabird_families" (
     UNIQUE ("image_id", "seabirdfamily_id")
 );
 ALTER TABLE "cms_image_seabird_families" ADD CONSTRAINT "image_id_refs_id_980bc4a2" FOREIGN KEY ("image_id") REFERENCES "cms_image" ("id") DEFERRABLE INITIALLY DEFERRED;
-
+COMMIT;
