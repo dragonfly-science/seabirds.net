7001e55256154233b709db95ad43dbdb9d92be53
Tim McNamara <code@timmcnamara.co.nz>
2012-04-10 16:24:10 +1200

    Moved logic from template to view; added tests

--- seabirds/profile/tests.py
+++ seabirds/profile/tests.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.utils import unittest
 from django.test.client import Client
+from categories.models import SeabirdFamily, InstitutionType
 from profile.models import UserProfile
 
 
@@ -9,17 +12,67 @@ class TestPages(unittest.TestCase):
 
     def test_model_supports_twitter(self):
         u = UserProfile()
-        self.assertTrue(hasattr(u, 'twitter_handle'))
+        self.assertTrue(hasattr(u, 'twitter'))
 
     def test_model_supports_twitter_optout(self):
         u = UserProfile()
-        self.assertTrue(hasattr(u, 'twitter_display_feed'))
+        self.assertTrue(hasattr(u, 'display_twitter'))
 
     def test_can_enter_twitter(self):
         response = self.c.get('/accounts/register')
-        twitter_in_form = 'twitter user name' in response.content.lower()
+        twitter_in_form = 'twitter' in response.content.lower()
         self.assertTrue(twitter_in_form)
+
     def test_can_opt_out_of_twitter_feed_being_displayed(self):
-        
         response = self.c.get('/accounts/register')
-        twitter_optout_in_form = 'display your twitter' in response.content.lower()
+        twitter_optout_in_form = 'Display your Twitter feed on your profile page' in response.content.lower()
+
+class TestCustomListView(unittest.TestCase):
+    fixtures = ['testdata.json']
+
+    def setUp(self):
+        self.c = Client()
+        self.c.login(username="alice", password="secrets")       
+
+    def test_anonymous_users_are_barred(self):
+        response = Client().get('/petrel', follow=True)
+        assert response.status_code is not 200
+
+    def test_logged_in_users_can_visit_page(self):
+        response1 = self.c.get('/petrel', follow=True)
+        response2 = self.c.get('/petrel/', follow=True)
+        assert response1.status_code ==  200, response.status_code
+        assert response2.status_code ==  200, response.status_code
+
+    def test_default_listing(self):
+        response = self.c.get('/petrel/', follow=True)
+        assert 'activebadge' not in response.content
+        assert 'activebadge' not in response.content
+
+    def test_country_no_seabirds_nor_collab_choices(self):
+        response = self.c.get('/petrel/?c=GB')
+        assert '<a href="/petrel/?c=SE" class="badge linkbadge">Sweden</a>' in response.content
+        assert '<a href="/petrel/" class="badge activebadge">United Kingdom</a>' in response.content
+        
+    def test_country_seabirds_but_no_collab_choices(self):
+        response = self.c.get('/petrel/?c=GB&s=alb')
+        assert '<a href="/petrel/?c=SE&s=alb" class="badge linkbadge">Sweden</a>' in response.content
+        assert '<a href="/petrel/?s=alb" class="badge activebadge">United Kingdom</a>' in response.content
+        assert '<a href="/petrel/?c=GB" class="badge activebadge">Albatrosses</a>' in response.content
+        assert '<a href="/petrel/?c=GB&s=auk" class="badge linkbadge">Auks</a>' in response.content
+
+    def test_country_seabirds_and_collab_choices(self):
+        response = self.c.get('/petrel/?c=GB&s=alb&r=graduate+student')
+        assert '<a href="/petrel/?c=SE&s=alb&r=graduate+student" class="badge linkbadge">Sweden</a>' in response.content
+        assert '<a href="/petrel/?s=alb&r=graduate+student" class="badge activebadge">United Kingdom</a>' in response.content
+        assert '<a href="/petrel/?c=GB&r=graduate+student" class="badge activebadge">Albatrosses</a>' in response.content
+        assert '<a href="/petrel/?c=GB&s=auk&r=graduate+student" class="badge linkbadge">Auks</a>' in response.content
+
+    def test_seabirds_no_countries_nor_collab_choices(self):
+        response = self.c.get('/petrel/?s=alb')
+        assert '<a href="/petrel/?c=SE&s=alb" class="badge linkbadge">Sweden</a>' in response.content
+        assert '<a href="/petrel/" class="badge activebadge">Albatrosses</a>' in response.content
+        
+
+    def test_seabirds_countries_and_collab_choices(self):
+        response = self.c.get('/petrel/?s=alb&r=graduate+student')
--- seabirds/profile/views.py
+++ seabirds/profile/views.py
@@ -1,3 +1,5 @@
+from urllib import urlencode
+
 from profiles.views import profile_detail
 from profiles import utils
 from django.http import Http404, HttpResponseRedirect
@@ -8,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django_countries.countries import COUNTRIES
 
 from categories.models import SeabirdFamily
-from profile.models import UserProfile
+from profile.models import UserProfile, CollaborationChoice
 
 @login_required
 def profile(request):
@@ -18,6 +20,7 @@ def profile(request):
         raise Http404
 
 COUNTRIES = dict(COUNTRIES)
+
 @login_required
 def custom_list(request, template_name='profiles/profile_list.html', **kwargs):
     """
@@ -28,8 +31,10 @@ def custom_list(request, template_name='profiles/profile_list.html', **kwargs):
     queryset = profile_model._default_manager.all()
     birds_queryset = profile_model._default_manager.all()
     countries_queryset = profile_model._default_manager.all()
-    extra_context = {'seabird': None, 'country':None}
+    collaboration_queryset = utils.get_collaboration_choice_model()._default_manager.all()
 
+    extra_context = {'seabird': None, 'country':None, 'country_tag':None, 'collaboration_choice':None}
+ 
     seabird = request.GET.get('s', None)
     if seabird is not None:
         try:
@@ -47,12 +52,66 @@ def custom_list(request, template_name='profiles/profile_list.html', **kwargs):
         extra_context['country'] =  unicode(COUNTRIES[country])
         extra_context['country_tag'] =  country
 
+    collab_choice = request.GET.get('r', None)
+    if collab_choice:
+        choice = CollaborationChoice.objects.get(id=collab_choice)
+        queryset = queryset.filter(collaboration_choices=choice)
+        collaboration_queryset = profile_model._default_manage.all()
+        extra_content['collaboration_choice'] = collaborator_choice_id
+
     kwargs['queryset'] = queryset.order_by('user__last_name')
     extra_context['profile_birds'] = SeabirdFamily.objects.filter(profiles__in=UserProfile.objects.all()).distinct()
     countries = list(set([(u.country.name, u.country) for u in UserProfile.objects.all()]))
     extra_context['profile_countries'] =  [c[1] for c in sorted(countries)]
+    extra_context['profile_collaboration_choices'] = CollaborationChoice.objects.filter(p)
+
+    def _record_details(r, queryset, record_type, activetest, country=None, seabird=None, collab_choice=None):
+        query_string_params = {}
+        r.tag = "a"
+        href = "/petrel/"
+        r.records = len(queryset.filter(**{record_type: r}))
+        classes = ["badge"]
+        if activetest(r.name) == extra_context['country']:
+            classes.append("activebadge")
+        elif r.records > 0:
+            classes.append("linkbadge")
+        else:
+           classes.extend(["smallbadge", "inactivebadge"])
+           r.tag = "span"
+
+        if country:
+            query_string_params['c'] = country
+        if seabird:
+            query_string_params['s'] = seabird[:3].lower()
+        if collab_choice:
+            query_string_params['r'] = collab_choice
+
+        query_string = urlencode(query_string_params)
+        if query_string:
+            href = href + '?' + query_string
+            r.href = 'href="{0}"'.format(href)
+        if r.records > 0:
+            r.href = ""
+        pc.classes = " ".join(classes)
+        return pc
+
+    def birds_activetest(bird_name):
+        return bird_name == extra_context['seabird']
+
+    def country_activetest(country_name):
+        return country_name == extra_context['country']
+
+    def collab_choice_activetest(collab_choice_id):
+        try:
+            c = CollaborationChoice.objects.get(pk=collab_choice_id)
+            return c.label == extra_context['collaboration_choice']
+        except CollaborationChoice.DoesNotExist:
+            return False
+
     for pb in extra_context['profile_birds']:
-        pb.records = len(birds_queryset.filter(seabirds = pb))
+        pb = _record_details(pb, birds_queryset, 'birds', birds_activetest, country, seabird, collab_choice)
     for pc in extra_context['profile_countries']:
-        pc.records = len(countries_queryset.filter(country = pc))
+        pc = _record_details(pc, countries_queryset, 'country', country_activetest, country, seabird, collab_choice)
+    for pr in extra_context['profile_collaboration_choices']:
+        pr = _record_details(pr, collaboration_queryset, 'collaboration_choice', collab_choice_activetest, country, seabird, collab_choice)
     return object_list(request, template_name=template_name, extra_context=extra_context, **kwargs)
