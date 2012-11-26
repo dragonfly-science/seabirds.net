c8b47c3276ad616dc7f13c506145524b6b72effc
Joel Pitt <joel@joelpitt.com>
2012-11-26 14:14:49 +1300

    Preserve the executable paths and use another environment variable to track the project dir

--- fabfile.py
+++ fabfile.py
@@ -12,12 +12,23 @@ env.production_server = 'seabirds.webfactional.com'
 env.local_user = os.environ['USER']
 
 env.hosts = [env.production_server]
-env.path = '/home/seabirds/seabirds.net'
-env.local_path = os.getcwd()
+env.remote_dir = '/home/seabirds/seabirds.net'
+env.local_dir = os.getcwd()
 
 #### Private functions ####
 @_contextmanager
 def _virtualenv():
+    """
+    Wrap functions in the virtualenv.
+
+    NOTE: this may not work at all if the virtualenv is already activated,
+    and won't work if the virtualenvwrapper.sh script is installed in the user's
+    .local dir.
+
+    TODO: It is often suggested to directly source the venv activation script:
+
+    http://stackoverflow.com/questions/1180411/activate-a-virtualenv-via-fabric-as-deploy-user
+    """
     local('/bin/bash /usr/local/bin/virtualenvwrapper.sh; workon seabirds')
     yield
     local('deactivate')
@@ -31,18 +42,21 @@ def git_pull():
 
 def get_secrets():
     "Get files that aren't in the checkout, such as sitesettings.py"
-    get('%(path)s/seabirds/sitesettings.py' % env, local_path='.')
-    get('%(path)s/seabirds/secrets.py' % env, local_path='seabirds')
+    # TODO: Should we really overwrite sitesettings.py?
+    # This would blast away information that a developer may have
+    # customised.
+    get('%(remote_dir)s/seabirds/sitesettings.py' % env, local_path='.')
+    get('%(remote_dir)s/seabirds/secrets.py' % env, local_path='seabirds')
 
 def get_live_media():
     "Copy media from the production server to the local machine"
-    local('rsync -avz -e "ssh -l seabirds" seabirds@%(production_server)s:%(path)s/seabirds/media %(local_path)s/seabirds/ --exclude=*.css --exclude=*.js' % env)
-    local('rsync -avzL -e "ssh -l seabirds" seabirds@%(production_server)s:%(path)s/static %(local_path)s/ --exclude=*.css --exclude=*.js' % env)
-    local('chown %(local_user)s:dragonfly %(local_path)s -R' % env)
+    local('rsync -avz -e "ssh -l seabirds" seabirds@%(production_server)s:%(remote_dir)s/seabirds/media %(local_dir)s/seabirds/ --exclude=*.css --exclude=*.js' % env)
+    local('rsync -avzL -e "ssh -l seabirds" seabirds@%(production_server)s:%(remote_dir)s/static %(local_dir)s/ --exclude=*.css --exclude=*.js' % env)
+    local('chown %(local_user)s:dragonfly %(local_dir)s -R' % env)
 
 def get_live_database():
     "Copy live database from the production server to the local machine"
-    with cd('%(path)s' % env):
+    with cd('%(remote_dir)s' % env):
         run('pg_dump -U seabirds -C seabirds > dumps/latest.sql')
         get('dumps/latest.sql', local_path='dumps')
         with settings(warn_only=True):
@@ -66,36 +80,36 @@ def pull():
 def git_push():
     "Make sure that any commits are synchronised with the server"
     local("git push")
-    with cd("%(path)s" % env):
+    with cd("%(remote_dir)s" % env):
         run('git pull')
 
 def put_secrets():
     "Put files that aren't in the checkout, such as sitesettings.py"
-    put('sitesettings.py', remote_path='%(path)s/seabirds' % env)
-    put('seabirds/secrets.py', remote_path='%(path)s/seabirds' % env)
+    put('sitesettings.py', remote_path='%(remote_dir)s/seabirds' % env)
+    put('seabirds/secrets.py', remote_path='%(remote_dir)s/seabirds' % env)
 
 def install():
     "Fetch any new software that is required and run syncdb"
-    with cd('%(path)s' % env):
+    with cd('%(remote_dir)s' % env):
         run('pip install -r requirements.txt')
 
 def update():
-    with cd('%(path)s' % env):
+    with cd('%(remote_dir)s' % env):
         run('pip install -r requirements.txt')
-    with cd('%(path)s/seabirds' % env):
+    with cd('%(remote_dir)s/seabirds' % env):
         run('python manage.py cuckoo run')
 
 def cuckoo():
-    with cd('%(path)s/seabirds' % env):
+    with cd('%(remote_dir)s/seabirds' % env):
         run('python manage.py cuckoo run')
 
 def syncdb():
-    with cd('%(path)s/seabirds' % env):
+    with cd('%(remote_dir)s/seabirds' % env):
         run('python manage.py syncdb')
 
 def validate():
     "Run django validation"
-    with cd('%(path)s/seabirds' % env):
+    with cd('%(remote_dir)s/seabirds' % env):
         run('python manage.py validate')
 
 def restart():
@@ -108,5 +122,3 @@ def push():
     update()
     validate()
     restart()
-
-
