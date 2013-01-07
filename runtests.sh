770df916a48ed043762bce074d3e9f46a1a50910
Joel Pitt <joel@joelpitt.com>
2013-01-08 09:42:48 +1300

    don't switch virtualenv since we don't have a way to know it's name

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,5 @@
-. ~/.virtualenvs/seabirds/bin/activate
+# Need a general way to specify what virtualenv to use
+#. ~/.virtualenvs/seabirds/bin/activate
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd DIR
 echo "Using virtualenv $VIRTUAL_ENV"
