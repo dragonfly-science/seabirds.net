4e26d3b2868c33ba41c8f8232d0e4df6d1acf17a
Edward Abraham <edward@dragonfly.co.nz>
2013-02-12 16:44:49 +1300

    Drop constraint

--- seabirds/sql-patches/2013-02-13-change-constraint.sql
+++ seabirds/sql-patches/2013-02-13-change-constraint.sql
@@ -2,5 +2,4 @@
 BEGIN;
 ALTER TABLE "cms_post" DROP CONSTRAINT "section_id_refs_id_ac56f035";
 DROP TABLE "cms_section";
-ALTER TABLE "cms_post" ADD CONSTRAINT "listing_id_refs_id_e67965c0" FOREIGN KEY ("listing_id") REFERENCES "cms_listing" ("id") DEFERRABLE INITIALLY DEFERRED;
 COMMIT;
